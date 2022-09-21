0325458b7539f65430178713dccabe3d29d64240
Adam Workman <adammworkman25@gmail.com>
2022-09-21 14:28:22 -0400

    refactor content

--- content.rb
+++ content.rb
@@ -1,18 +1,27 @@
 require 'rspec'
 
 MULTIPLIER = 0.877
-@terpenes = {}
+
+def print_content(content_hash)
+  print("THC potential: #{thc_content(content_hash)} CBD potential: #{cbd_content(content_hash)} Terp content: #{terp_content(content_hash)} Terpenes: #{@terpenes.keys.join(",")}")
+end
+
+private
+
+def cannabinoid_content(content_hash, cannabinoid)
+  float_to_percentage(content_hash["#{cannabinoid}"] + (MULTIPLIER * content_hash["#{cannabinoid}A"]))
+end
 
 def float_to_percentage(float)
   '%.2f' %  (float * 100)
 end
 
 def thc_content(content_hash)
-  float_to_percentage(content_hash['THC'] + (MULTIPLIER * content_hash['THCA']))
+  cannabinoid_content(content_hash, 'THC')
 end
 
 def cbd_content(content_hash) 
-  float_to_percentage(content_hash['CBD'] + (MULTIPLIER * content_hash['CBDA']))
+  cannabinoid_content(content_hash, 'CBD')
 end
 
 def terp_content(content_hash)
@@ -20,9 +29,6 @@ def terp_content(content_hash)
   float_to_percentage(@terpenes.values.inject(0, :+))
 end
 
-def print_content(content_hash)
-  print("THC potential: #{thc_content(content_hash)} CBD potential: #{cbd_content(content_hash)} Terp content: #{terp_content(content_hash)} Terpenes: #{@terpenes.keys.join(",")}")
-end
 
 # TEST
 
@@ -37,24 +43,6 @@ describe 'content calculator' do
       'LEMONENE' => 0.04000,
       'TERPINEOL' => 0.06600
     }
-  end
-
-  context 'thc content' do
-    it 'returns the thc content' do
-      expect(thc_content(CONTENT_HASH)).to eq('16.33')
-    end
-  end
-
-  context 'cbd content' do
-    it 'returns the cbd content' do
-      expect(cbd_content(CONTENT_HASH)).to eq('6.75')
-    end
-  end
-
-  context 'terp content' do
-    it 'returns the terp content' do
-      expect(terp_content(CONTENT_HASH)).to eq('10.60')
-    end
   end
 
   context 'print_content' do
